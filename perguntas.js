aadf077682ba0885bf9da22510310ab7a89534c8
luaninhagameplays <lu02ana07@gmail.com>
2024-11-12 19:56:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,101 @@
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais próximo do Sol?',
+    'Mercúrio.'
+)
+
+criaCartao(
+    'Atualidades',
+    'Qual foi o país-sede da Copa do Mundo de Futebol de 2022?',
+    'Catar (Qatar).'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a soma dos ângulos internos de um triângulo?',
+    '180 graus.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico do elemento Ouro?',
+    'Au.'
+)
+
+
+
+
+criaCartao(
+    'Biologia',
+    'Qual organela celular é responsável pela produção de energia na célula?',
+    'Mitocôndria.'
+)
+
+
+
+
+
+
+
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Rússia.'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+criaCartao(
+    'Cultura Geral',
+    'Qual país é conhecido como "a terra do sol nascente"?',
+    'Japão.'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+criaCartao(
+    'Curiosidade - Astronomia',
+    'Qual é o único planeta do Sistema Solar que gira no sentido horário?',
+    'Vênus.'
+)
+
+
+
+
+
+
+
+
+
+
+criaCartao(
+    'Curiosidade - Animais',
+    'Qual animal consegue dormir até três anos seguidos?',
+    'O caracol.'
+)
